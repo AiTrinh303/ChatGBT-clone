bb3e83a52d6fda2f30e6bdcdbc6295b59c2b2182
AiTrinh303 <trong.trinh@dci-student.org>
2024-08-05 22:35:49 +0200

    add pakage.json

---
+++ .history/client/src/main_20240805201105.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Homepage from "./routes/homepage/Homepage";
+import DashboardPage from "./routes/dashboardPage/DashboardPage";
+import ChatPage from "./routes/chatPage/ChatPage";
+import RootLayout from "./layouts/rootLayout/RootLayout";
+import DashboardLayout from "./layouts/dashboardLayout/DashboardLayout";
+import SignInPage from "./routes/signInPage/SignInPage";
+import SignUpPage from "./routes/signUpPage/SignUpPage";
+import 'dotenv/config';
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
+
+if(process.env.VITE_API_URL === "production") disableReactDevTools();
+
+const router = createBrowserRouter([
+  {
+    element: <RootLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Homepage />,
+      },
+      {
+        path: "/sign-in/*",
+        element: <SignInPage />,
+      },
+      {
+        path: "/sign-up/*",
+        element: <SignUpPage />,
+      },
+      {
+        element: <DashboardLayout />,
+        children: [
+          {
+            path: "/dashboard",
+            element: <DashboardPage />,
+          },
+          {
+            path: "/dashboard/chats/:id",
+            element: <ChatPage />,
+          },
+        ], 
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
