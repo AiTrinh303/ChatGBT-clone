fb1c356eddef4a22a0acc8e8ee4806b85e1b5c72
AiTrinh303 <trong.trinh@dci-student.org>
2024-08-05 16:03:10 +0200

    Fixed env file & change style

---
+++ .history/client/src/routes/dashboardPage/DashboardPage_20240805150217.jsx
@@ -0,0 +1,79 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import "./dashboardPage.css";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "@clerk/clerk-react";
+
+const DashboardPage = () => {
+  // const queryClient = useQueryClient();
+
+  // const navigate = useNavigate();
+
+  // const mutation = useMutation({
+  //   mutationFn: (text) => {
+  //     return fetch(`${import.meta.env.VITE_API_URL}/api/chats`, {
+  //       method: "POST",
+  //       credentials: "include",
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //       body: JSON.stringify({ text }),
+  //     }).then((res) => res.json());
+  //   },
+  //   onSuccess: (id) => {
+  //     // Invalidate and refetch
+  //     queryClient.invalidateQueries({ queryKey: ["userChats"] });
+  //     navigate(`/dashboard/chats/${id}`);
+  //   },
+  // });
+  
+  
+  
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const text = e.target.text.value;
+    if (!text) return;
+    // mutation.mutate(text);
+    await fetch (`${import.meta.env.VITE_API_URL}/api/chats/`, {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({text}),
+    })
+  };
+  return (
+    <div className="dashboardPage">
+      <div className="texts">
+        <div className="logo">
+          <span>💫🌎🪐 <h1>A.I. PLANET</h1> 🪐🌎💫 </span>
+        </div>
+        <div className="options">
+          <div className="option">
+            <img src="/chat.png" alt="" />
+            <span>Create a New Chat</span>
+          </div>
+          <div className="option">
+            <img src="/image.png" alt="" />
+            <span>Analyze Images</span>
+          </div>
+          <div className="option">
+            <img src="/code.png" alt="" />
+            <span>Help me with my Code</span>
+          </div>
+        </div>
+      </div>
+
+      <div className="formContainer">
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="text" placeholder="Ask me anything ..." />
+          <button>
+            <img src="/arrow.png" alt="" />
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default DashboardPage
