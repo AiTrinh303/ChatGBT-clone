20667fe391be49cd084d492198c1d170e8977240
AiTrinh303 <trong.trinh@dci-student.org>
2024-09-09 22:58:30 +0200

    changed some file to deploying

--- .history/backend/index_20240727151340.js
+++
@@ -1,2 +0,0 @@
-import express from "express";
-import cors from "cors";
--- .history/backend/index_20240727151442.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import cors from "cors";
-import 'dotenv/config';
-
-const port = process.env.PORT || 3000;
-const app = express();
-app.use(express.json());
